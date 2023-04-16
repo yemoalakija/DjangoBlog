e48be319dd98748a4de5d29c2a6c9f82f5ed8ad3
yemoalakija <adeyemo.alakija@gmail.com>
2023-04-16 21:47:40 +0000

    Create admin, summernote, and urls

--- storyline/admin.py
+++ storyline/admin.py
@@ -1,3 +1,20 @@
+"""
+The Django Admin Site
+This module provides the administrative interface for Django web applications.
+It allows authorized users to create, view, edit, and delete content from the
+application's database through a user-friendly interface. The admin site can
+be customized by registering models and creating custom admin classes that
+define how each model should be displayed and edited.
+For more information, see the Django documentation:
+https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
+"""
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """Docstring"""
+    summernote_fields = ("content")
--- yemos/settings.py
+++ yemos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django_summernote',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
