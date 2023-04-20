7bfee536ca3a0b0989e196f68aab72d46b64ad28
yemoalakija <adeyemo.alakija@gmail.com>
2023-04-20 01:13:22 +0000

    Install allauth; fix settings, urls; and fix base html

--- yemos/settings.py
+++ yemos/settings.py
@@ -44,12 +44,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'storyline',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- yemos/urls.py
+++ yemos/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('storyline.urls'), name='storyline_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
