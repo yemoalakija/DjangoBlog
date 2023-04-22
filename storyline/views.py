63d242127715c8399d27189d60b0c0e38bdec81e
yemoalakija <adeyemo.alakija@gmail.com>
2023-04-22 03:06:16 +0000

    Modify views code

--- storyline/views.py
+++ storyline/views.py
@@ -5,9 +5,8 @@ from .models import Post
 from .forms import CommentForm
 
 
-# Create your views here.
 class PostList(generic.ListView):
-    """Creates view"""
+    """List view for the posts."""
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
@@ -15,14 +14,14 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-    """Displays post"""
+    """Displays the detail of the post."""
     def get(self, request, slug, *args, **kwargs):
         """Get the post to read"""
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("created_on")
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
 
         return render(
@@ -43,9 +42,9 @@ class PostDetail(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("created_on")
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
-        
+
         comment_form = CommentForm(data=request.POST)
 
         if comment_form.is_valid():
@@ -65,6 +64,6 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": True,
                 "liked": liked,
-                "comment_form": CommentForm(),
+                "comment_form": comment_form,
             },
         )
