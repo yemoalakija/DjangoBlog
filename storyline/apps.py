f4f53886d55706e9876a68cdafebd29888035ce6
yemoalakija <adeyemo.alakija@gmail.com>
2023-04-24 00:24:56 +0000

    Docstring apps

--- storyline/apps.py
+++ storyline/apps.py
@@ -1,6 +1,8 @@
+"""App config"""
 from django.apps import AppConfig
 
 
 class StorylineConfig(AppConfig):
+    """Initialises app config"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'storyline'
