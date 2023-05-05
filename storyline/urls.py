1fe4115197c9acb2330115f9a16345c167c4042f
yemoalakija <>
2023-05-05 22:01:06 +0000

    Improving the UX

--- storyline/urls.py
+++ storyline/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("<slug>:slug/", views.PostDetail.as_view(), name="post_detail"),
     path("like/<slug>:slug/", views.PostLike.as_view(), name="post_like"),
-    path("like_post/", views.PostLike.like_post, name="like_post"),
 ]
