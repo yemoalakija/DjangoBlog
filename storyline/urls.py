30d24f57448229c6400b3b8bbe32e98cc43dd51e
yemoalakija <>
2023-05-06 22:27:07 +0000

    Views and base fix

--- storyline/urls.py
+++ storyline/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
-    path("<slug>:slug/", views.PostDetail.as_view(), name="post_detail"),
-    path("like/<slug>:slug/", views.PostLike.as_view(), name="post_like"),
+    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+    path("like/<slug:slug>/", views.PostLike.as_view(), name="post_like"),
 ]
