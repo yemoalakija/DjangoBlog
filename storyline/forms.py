bc0e6634bcd2636faff056842d24dbda2781195b
yemoalakija <>
2023-05-07 01:54:21 +0000

    Expand messaging error

--- storyline/forms.py
+++ storyline/forms.py
@@ -9,3 +9,10 @@ class CommentForm(forms.ModelForm):
         """Defines form model and field"""
         model = Comment
         fields = ("body",)
+    
+    def clean_body(self, request):
+        """Ensure comment body is not empty or whitespace-only"""
+        body = self.cleaned_data["body"]
+        if not body.strip():
+            raise forms.ValidationError("Comment cannot be empty or contain only whitespace.")
+        return body
