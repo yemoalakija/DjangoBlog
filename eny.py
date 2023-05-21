e71c577cf3eacf9df56822a8af3181b1b8022fcc
yemoalakija <adeyemo.alakija@gmail.com>
2023-05-21 09:00:54 +0000

    Remove env file

--- eny.py
+++
@@ -1,6 +0,0 @@
-"""Routine module"""
-import os
-
-os.environ["DATABASE_URL"] = "postgres://nxognlfu:DWw1HitvmT9T6xEK5BKE00iL7kajDQ9t@kandula.db.elephantsql.com/nxognlfu"
-os.environ["YEMOS_SECRET_KEY"] = "django-insecure-fw%py7x3mzi3-72arnqr#ol&cn4w$)$*isay5xttz-#)qa0%zk"
-os.environ["CLOUDINARY_URL"] = "cloudinary://517136666661473:kP20hz80KrMNrOgIh-Hto0errC4@dzr5arhcu"
